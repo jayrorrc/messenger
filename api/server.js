a87981b0a3c423740e8166b61e92d20d193526b4
Jayro Rafael Ribeiro Camelo <jayrorrc@gmail.com>
2020-06-21 19:32:43 -0300

    [0.3.0] Set up database

--- api/server.js
+++ api/server.js
@@ -1,13 +1,30 @@
 'use strict';
 
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(logger('dev'));
+
+const mongoConfig = require('./mongo_config.json');
+// set up mongoose
+mongoose.connect('mongodb://' + mongoConfig.url + ':' + mongoConfig.port + '/' + mongoConfig.database)
+    .then(() => {
+        console.log('Database connected');
+    })
+    .catch((error) => {
+        console.log('Error connecting to database');
+    });
 
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
 // App
-const app = express();
 app.get('/', (req, res) => {
     res.send('We have an api!!!');
 });
